47beb68ed505fb127b8190f6865a36f3d2e5170a
Ishmam <techopoly444@gmail.com>
2022-04-29 17:36:44 +0600

    deploying to heroku

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use('/', (req, res, next) => {
 
 
 mongoConnect(async () => {
-    app.listen(5000, () => console.log('Server running on port 5000!'))
+    app.listen(process.env.PORT || 5000, () => console.log('Server running on port 5000!'))
 
 });
 
